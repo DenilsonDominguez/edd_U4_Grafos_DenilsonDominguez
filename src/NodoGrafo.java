019d8ba9746c30440fa6d258006b0c5753281be6
DenilsonDominguez <82048725+DenilsonDominguez@users.noreply.github.com>
2021-06-12 01:35:06 -0700

    Update NodoGrafo.java

--- src/NodoGrafo.java
+++ src/NodoGrafo.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author chuy4
- */
+//Denilson de Jesus Dominguez Herrera
 public class NodoGrafo {
     char valor;
     NodoGrafo ant, sig;
